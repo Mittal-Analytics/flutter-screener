9937670581e0e73558d8afff239faea658add737
KaviHarjani <kavi_harjani@hotmail.com>
2022-01-31 09:00:07 +0530

    diff by Gobind

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,48 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(ScreenerWebView());
+void main() {
+  runApp(ScreenerApp());
+}
 
-class ScreenerWebView extends StatelessWidget {
+class ScreenerApp extends StatelessWidget {
+  const ScreenerApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Screener',
-      home: Scaffold(
-        appBar: PreferredSize(
-            preferredSize: Size.fromHeight(0),
-            child: AppBar(
-              // ignore: prefer_const_constructors
-              title: SizedBox(
-                height: kToolbarHeight,
-              ),
-            )),
-        // ignore: prefer_const_constructors
-        body: WebView(
-          initialUrl: "https://www.screener.in/",
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
+      home: ScreenerHome(),
+    );
+  }
+}
+
+class ScreenerHome extends StatefulWidget {
+  const ScreenerHome({Key? key}) : super(key: key);
+
+  @override
+  _ScreenerHomeState createState() => _ScreenerHomeState();
+}
+
+class _ScreenerHomeState extends State<ScreenerHome> {
+  @override
+  Widget build(BuildContext context) {
+    const _screenerHomeUrl = "https://www.screener.in";
+    const _proxyUserAgent =
+        "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(0),
+          child: AppBar(
+            title: const SizedBox(
+              height: kToolbarHeight,
+            ),
+          )),
+      body: WebView(
+        initialUrl: _screenerHomeUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+        userAgent: _proxyUserAgent,
       ),
     );
   }
