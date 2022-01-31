73082bd9c987650a7adcc3722b86e7d0add8dc83
KaviHarjani <kavi_harjani@hotmail.com>
2022-01-31 10:09:19 +0530

    Converting it all to stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,40 +12,25 @@ class ScreenerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Screener',
-      home: ScreenerHome(),
-    );
-  }
-}
-
-class ScreenerHome extends StatefulWidget {
-  const ScreenerHome({Key? key}) : super(key: key);
-
-  @override
-  _ScreenerHomeState createState() => _ScreenerHomeState();
-}
-
-class _ScreenerHomeState extends State<ScreenerHome> {
-  @override
-  Widget build(BuildContext context) {
     const _screenerHomeUrl = "https://www.screener.in";
     const _proxyUserAgent =
         "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
-    return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size.fromHeight(0),
-          child: AppBar(
-            title: const SizedBox(
-              height: kToolbarHeight,
-            ),
-          )),
-      body: WebView(
-        initialUrl: _screenerHomeUrl,
-        javascriptMode: JavascriptMode.unrestricted,
-        userAgent: _proxyUserAgent,
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Screener',
+        home: Scaffold(
+          appBar: PreferredSize(
+              preferredSize: Size.fromHeight(0),
+              child: AppBar(
+                title: const SizedBox(
+                  height: kToolbarHeight,
+                ),
+              )),
+          body: WebView(
+            initialUrl: _screenerHomeUrl,
+            javascriptMode: JavascriptMode.unrestricted,
+            userAgent: _proxyUserAgent,
+          ),
+        ));
   }
 }
