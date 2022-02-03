a4e0b62fa0054c435cc56012778dec28e0ac5884
manujagobind <manujagobind@gmail.com>
2022-02-03 18:52:42 +0530

    Refactor app into smaller widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(ScreenerApp());
+  runApp(const ScreenerApp());
 }
 
 class ScreenerApp extends StatelessWidget {
@@ -13,34 +11,54 @@ class ScreenerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const _appTitle = "Screener";
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: _appTitle,
+        home: ScreenerHome(),
+    );
+  }
+}
+
+class ScreenerHome extends StatelessWidget {
+  const ScreenerHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(0),
+          child: AppBar(
+            title: const SizedBox(
+              height: kToolbarHeight,
+            ),
+          )),
+      body: const ScreenerWebView(),
+    );
+  }
+}
+
+class ScreenerWebView extends StatelessWidget {
+  const ScreenerWebView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     const _screenerHomeUrl = "https://www.screener.in";
     const _proxyUserAgent =
         "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Screener',
-        home: Scaffold(
-          appBar: PreferredSize(
-              preferredSize: Size.fromHeight(0),
-              child: AppBar(
-                title: const SizedBox(
-                  height: kToolbarHeight,
-                ),
-              )),
-          body: WebView(
-            initialUrl: _screenerHomeUrl,
-            javascriptMode: JavascriptMode.unrestricted,
-            userAgent: _proxyUserAgent,
-            navigationDelegate: (NavigationRequest request) {
-              if (request.url.startsWith("https://www.screener.in/")) {
-                return NavigationDecision.navigate;
-              } else {
-                _launchURL(request.url);
-                return NavigationDecision.prevent;
-              }
-            },
-          ),
-        ));
+    return WebView(
+      initialUrl: _screenerHomeUrl,
+      javascriptMode: JavascriptMode.unrestricted,
+      userAgent: _proxyUserAgent,
+      navigationDelegate: (NavigationRequest request) {
+        if (request.url.startsWith(_screenerHomeUrl)) {
+          return NavigationDecision.navigate;
+        } else {
+          _launchURL(request.url);
+          return NavigationDecision.prevent;
+        }
+      },
+    );
   }
 }
 
